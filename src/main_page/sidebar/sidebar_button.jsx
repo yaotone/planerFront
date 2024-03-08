95a8c0553172568d261bce62016d4d2f92389360
Gulyaev Danila <danili4in@gmail.com>
2024-03-08 12:13:14 +0300

    Баг фикс предыдущего коммита

--- src/main_page/sidebar/sidebar_button.jsx
+++ src/main_page/sidebar/sidebar_button.jsx
@@ -1,26 +1,27 @@
 import './sidebar_button.css'
 
 export default function SidebarButton({onClick, image, children, style, amount, isActive}){
-    let buttonClass;
-    if((isActive === children) && 
-    document.querySelector('.sidebar').classList.contains('active_sidebar')){
-        buttonClass = 'sidebar_button active_button sidebar_button_show';
-    }
-    else if(!(isActive === children) && 
-    document.querySelector('.sidebar').classList.contains('active_sidebar')){
-        buttonClass = 'sidebar_button sidebar_button_show'
-    }
-    else if((isActive === children) && 
-    !document.querySelector('.sidebar').classList.contains('active_sidebar')){
-        buttonClass = 'sidebar_button active_button'
-    }
-    else{
-        buttonClass = 'sidebar_button'
-    }
+//     console.log(document.querySelector('.sidebar').classList);
+//     let buttonClass;
+//     if((isActive === children) && 
+//     document.querySelector('.sidebar').classList.contains('active_sidebar')){
+//         buttonClass = 'sidebar_button active_button sidebar_button_show';
+//     }
+//     else if(!(isActive === children) && 
+//     document.querySelector('.sidebar').classList.contains('active_sidebar')){
+//         buttonClass = 'sidebar_button sidebar_button_show'
+//     }
+//     else if((isActive === children) && 
+//     !document.querySelector('.sidebar').classList.contains('active_sidebar')){
+//         buttonClass = 'sidebar_button active_button'
+//     }
+//     else{
+//         buttonClass = 'sidebar_button'
+//     }
 
     return(
         <div className='column_sidebar' style={{marginTop:`${style}px`}}>
-            <button onClick={onClick} className={buttonClass}>
+            <button onClick={onClick} className={(isActive === children) ? 'sidebar_button active_button' : 'sidebar_button'}>
                 <div className='left'>
                     <img src={image} alt={image} />
                     <span>{children}</span>
